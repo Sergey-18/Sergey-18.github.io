a8bd999b386c77700c7558991fb654e675cd37e5
Сергей Ротанов <sergejrotanov@MacBook-Air-Sergej.local>
2023-05-12 01:45:09 +0300

    add section tabs

---
+++ Run_smart/src/js/tab.js
@@ -0,0 +1,23 @@
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass
+      ('catalog__content_active').eq($(this).index()).addClass
+      ('catalog__content_active');
+  });
+
+
+// выпадающий список внутри карточки 
+
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
